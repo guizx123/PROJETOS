66ad5fe47d3eba5f468c108d6dc5d2e94ace1746
guizx123 <guifessel1@gmail.com>
2026-01-12 12:01:45 -0300

    Adiciona projeto clonenetflix

---
+++ clonenetflix/src/components/Form/Form.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, } from "react-native";
+import { styles } from "./SForm";
+
+type FormProps = {
+    onSuccess: () => void;
+};
+
+export default function Form({ onSuccess }: FormProps) {
+
+    const [usuario, setUsuario] = useState("");
+    const [senha, setSenha] = useState("")
+    const [login, setLogin] = useState("Preencha os campos")
+
+    function Validador() {
+        if (usuario != "" && senha != "") {
+            setLogin("Você logou")
+            onSuccess()
+            setUsuario("")
+            setSenha("")
+
+            return
+        }
+
+        setUsuario("")
+        setSenha("")
+        setLogin("Preencha os campos")
+
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.contentContainer}>
+
+                <Text style={styles.Text}>Usuário:</Text>
+                <TextInput style={styles.input}
+                    onChangeText={setUsuario}
+                    value={usuario}
+                    maxLength={10}
+
+                />
+                <Text style={styles.Text}>Senha:</Text>
+                <TextInput style={styles.input}
+                    onChangeText={setSenha}
+                    value={senha}
+                    maxLength={10}
+                    secureTextEntry={true}
+                    textContentType="password"
+
+                />
+                <TouchableOpacity style={styles.Button}
+                    onPress={Validador}>
+                    <Text style={styles.TextButton}>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+            <Text>{login}</Text>
+        </View>
+    );
+}
---
+++ clonenetflix/src/screens/Login.tsx
@@ -0,0 +1,28 @@
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import Header from "../components/header/Header";
+import Form from "../components/Form/Form";
+import { router } from "expo-router"
+import { styles } from "./SLogin";
+
+
+export default function Login() {
+    function loginSucess() {
+        router.replace("/Home");
+    }
+
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                <View style={styles.headerArea}>
+                    <Header />
+                </View>
+                <View style={styles.formArea}>
+                    <Form onSuccess={loginSucess} />
+                </View>
+            </View>
+        </SafeAreaView>
+
+    );
+}
